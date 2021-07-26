0232b92fb78f64c167411c6e11c2a0cceb6d52af
flopown <thijsbenjamin@live.be>
2021-07-26 15:46:41 +0200

    adding setup file for flask install

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Paranuara',
+    version='1.0',
+    author='Benjamin Thijs',
+    author_email='thijsbenjamin@live.be',
+    packages=[
+        'Flask'
+    ]
+)
